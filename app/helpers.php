2c4ae72c693c8e073d3cd550419cd9d97ea826b5
Sabuj <sabujscp.311996@gmail.com>
2021-02-18 14:45:30 +0600

    notification system

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Auth;
 
+use Carbon\Carbon;
+
 
 function package($id)
 {
@@ -11,7 +13,44 @@ function package($id)
 
 function newusernotification()
 {
-    // return $id;
-    return App\Models\CustomerModel::where('user_id', null)->get();
+    $notifications =[];
+
+
+    $datas = App\Models\CustomerModel::orderBy('updated_at', 'asc')->get();
+
+    $count = 0;
+    foreach ($datas as $key => $data) {
+        if($data->user_id == '' ){
+            // return (Carbon::now()->diffInHours($data->created_at))/24;
+            $create_notification = [
+                'id' => $data->id,
+                'username' => $data->name,
+                'message' => 'New User, Active Now',
+                'ago' => $data->created_at->diffForHumans(),
+                'messagetype' => 'newuser',
+                // 'date' => (Carbon::now()->diffInHours($data->updated_at))/24,
+            ];
+            array_push($notifications, $create_notification);
+        }
+        if(((Carbon::now()->diffInHours($data->updated_at))/24)>2 && $data->status == 3){
+            $create_notification = [
+                'id' => $data->id,
+                'username' => $data->name,
+                'message' => 'Date Expire',
+                'ago' => $data->updated_at->diffForHumans(),
+                'messagetype' => 'dateexpire',
+            ];
+            array_push($notifications, $create_notification);
+        }
+    }
+
+
+
+    
+    // $paymant = App\Models\CustomerModel::all();
+    // return $time;
+    return $notifications;
+    // return $newusers = App\Models\CustomerModel::orderBy('updated_at', 'desc')->get();
 }
+
 
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -335,111 +335,28 @@
                 </a>
                 <h4 class="">Notifications</h4>
                 <div class="notification-list nicescroll">
+
+                    {{-- {{ newusernotification() }} --}}
                     
                     <ul class="list-group list-no-border user-list">
                         @foreach (newusernotification() as $user)
                             <li class="list-group-item">
-                                <a href="{{ route('customer.newlist') }}" class="user-list-item">
+                                {{-- <a href="{{ route('customer.newlist') }}" class="user-list-item"> --}}
+                                <a href="{{ ($user['messagetype'] == 'newuser') ? route('customer.newlist') : route('customer.inactivelist') }}" class="user-list-item">
                                     <div class="icon bg-info">
-                                        <i class="mdi mdi-account"></i>
+                                        <i class="{{ ($user['messagetype'] == 'newuser') ? 'mdi mdi-account' : 'fas fa-file-invoice' }}"></i>
                                     </div>
                                     <div class="user-desc">
-                                        <span class="name">{{ $user->name }}</span>
-                                        <span class="desc">Active  user</span>
-                                        <span class="time">{{ $user->created_at->diffForHumans() }}</span>
+                                        <span class="name">{{ $user['username'] }}</span>
+                                        <span class="desc">{{ $user['message'] }}</span>
+                                        <span class="time">{{ $user['ago'] }}</span>
                                     </div>
                                 </a>
                             </li>
                         @endforeach
                     </ul>
                     
-                    <ul class="list-group list-no-border user-list">
-                        <li class="list-group-item">
-                            <a href="#" class="user-list-item">
-                                <div class="avatar">
-                                    <img src="{{ asset('admin/assets/images/users/avatar-2.jpg') }}" alt="">
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">Michael Zenaty</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">2 hours ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="#" class="user-list-item">
-                                <div class="avatar">
-                                    <img src="{{ asset('admin/assets/images/users/avatar-2.jpg') }}" alt="">
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">Michael Zenaty</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">2 hours ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="#" class="user-list-item">
-                                <div class="avatar">
-                                    <img src="{{ asset('admin/assets/images/users/avatar-2.jpg') }}" alt="">
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">Michael Zenaty</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">2 hours ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="#" class="user-list-item">
-                                <div class="icon bg-info">
-                                    <i class="mdi mdi-account"></i>
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">New Signup</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">5 hours ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-group-item">
-                            <a href="#" class="user-list-item">
-                                <div class="icon bg-pink">
-                                    <i class="mdi mdi-comment"></i>
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">New Message received</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">1 day ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-group-item active">
-                            <a href="#" class="user-list-item">
-                                <div class="avatar">
-                                    <img src="assets/images/users/avatar-3.jpg" alt="">
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">James Anderson</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">2 days ago</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="list-group-item active">
-                            <a href="#" class="user-list-item">
-                                <div class="icon bg-warning">
-                                    <i class="mdi mdi-settings"></i>
-                                </div>
-                                <div class="user-desc">
-                                    <span class="name">Settings</span>
-                                    <span class="desc">There are new settings available</span>
-                                    <span class="time">1 day ago</span>
-                                </div>
-                            </a>
-                        </li>
-
-                    </ul>
+                    
                 </div>
             </div>
             <!-- /Right-bar -->
